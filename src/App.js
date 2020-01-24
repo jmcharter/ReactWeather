7889f9e75cc779f6e966c2b17c5455fc25e3920f
Jon Charter <jmcharter@gmail.com>
2020-01-24 15:53:15 +0000

    added click counter to test, added search bar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+
+const API_Key = "5311918c1c662f293ab051013c9773de"
 
 function App() {
+
+  const [clickCount, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="outer">
+        <div className="header">
+          <SearchBar />
+        </div>
+        <div className="body">
+          <button onClick={() => setCount(clickCount + 1)} className="countbtn">Click to count</button>
+          <p className="counter">You've clicked {clickCount} times!</p>
+        </div>
+        <div className="footer"></div>
+      </div>
     </div>
   );
 }
