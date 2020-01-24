8e289437302d3b31e93a9409bf043dc77524a845
Jon Charter <jmcharter@gmail.com>
2020-01-24 15:48:26 +0000

    created search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SearchBar() {
+    return (
+        <div className="search-container">
+            <input
+                type="text"
+                className="searchbar"
+                placeholder="Location..."
+
+            />
+        </div>
+    )
+};
+
+export default SearchBar
