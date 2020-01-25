3e6ef3238cef1e2f2570ef368ddcb507172f1f6f
Jon Charter <jmcharter@gmail.com>
2020-01-25 22:51:12 +0000

    moved component imports and api call to Search.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import Clock from './components/Clock';
+import Search from './components/Search';
 
-const API_Key = "5311918c1c662f293ab051013c9773de"
 
 function App() {
-
-  const [clickCount, setCount] = useState(0);
 
   return (
     <div className="app">
       <div className="outer">
         <div className="header">
-          <SearchBar />
-        </div>
-        <div className="body">
-          <Clock />
+          <Search />
         </div>
         <div className="footer"></div>
       </div>
-    </div>
+    </div >
   );
 }
 
